d56e64e23ba39ae4a21f626de8c2264f3d01f497
“jksurampudi5” <jksurampudi5@gmail.com>
2026-09-20 22:30:05 +0530

    Use final Android package name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'om.findlostpuppy.app',
+  appId: 'com.findlostpuppy.app',
   appName: 'findlostpuppy',
   webDir: 'dist',
   server: {
